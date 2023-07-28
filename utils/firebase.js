e6eb7387800bcb9b0f2e18974b32cedbd74f30fb
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-07-28 14:54:49 +0100

    general setup

---
+++ utils/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import {getFireStore} from "firebase/firestore"
+import {getStorage} from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClKyBzj2DaHlOjtZHf-Pfa4jR-LwQ_OOY",
+  authDomain: "virtual-bank-a93fa.firebaseapp.com",
+  projectId: "virtual-bank-a93fa",
+  storageBucket: "virtual-bank-a93fa.appspot.com",
+  messagingSenderId: "317155963180",
+  appId: "1:317155963180:web:c66bcf2a0f08963b6d7c89"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFireStore(app)
+export const storage = getStorage(app)
+export default app 
